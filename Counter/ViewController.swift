3a868c8db6add131bfadc06744756e324b6cc534
a.krasnov <wonsarc@icloud.com>
2023-07-23 16:32:36 +0300

    add animation

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var decrementButton: UIButton!
-    @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var resetCountButton: UIButton!
+    @IBOutlet weak private var incrementButton: UIButton!
+    @IBOutlet weak private var decrementButton: UIButton!
+    @IBOutlet weak private var resultLabel: UILabel!
+    @IBOutlet weak private var historyTextView: UITextView!
+    @IBOutlet weak private var resetCountButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,24 +34,59 @@ class ViewController: UIViewController {
         return "\n\(currentDate): значение изменено на \(count)"
     }
     
-    @IBAction func plusCount(_ sender: Any) {
+    @IBAction private func plusCount(_ sender: UIButton) {
+        playAnimateButton(sender: sender)
         countResult+=1
         resultLabel.text = messageResult
-        historyTextView.text += getHistoryMessage(count: "+1")    }
+        historyTextView.text += getHistoryMessage(count: "+1")
+        scrollTextViewToBottom(textView: historyTextView)
+    }
     
-    @IBAction func minusCount(_ sender: Any) {
+    @IBAction private func minusCount(_ sender: UIButton) {
+        playAnimateButton(sender: sender)
         if countResult == 0 {
             historyTextView.text += "\n\(currentDate): попытка уменьшить значение счётчика ниже 0"
+            scrollTextViewToBottom(textView: historyTextView)
             return
         }
         countResult-=1
         resultLabel.text = messageResult
         historyTextView.text += getHistoryMessage(count: "-1")
+        scrollTextViewToBottom(textView: historyTextView)
     }
     
-    @IBAction func resetCount() {
+    @IBAction private func resetCount() {
         countResult = 0
         resultLabel.text = "0"
         historyTextView.text += "\n\(currentDate): значение сброшено"
+        scrollTextViewToBottom(textView: historyTextView)
+    }
+    
+    private func scrollTextViewToBottom(textView: UITextView) {
+        UIView.animate(withDuration: 0.3) {
+            textView.alpha = 0.5
+        }
+        
+        let bottom = NSMakeRange(textView.text.count - 1, 1)
+        textView.scrollRangeToVisible(bottom)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                textView.alpha = 1.0
+            }
+        }
+    }
+    
+    private func playAnimateButton(sender: UIButton) -> Void {
+        UIView.animate(withDuration: 0.3) {
+            sender.alpha = 0.5
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+            }
+        }
     }
 }
+
